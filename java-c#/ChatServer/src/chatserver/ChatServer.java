0fecf570f09a2ea837f5b05b764f7d3c3f6d58d3
Alexander Russell (ajr546) <alexander.james@usask.ca>
2017-10-13 11:41:23 -0600

    runnable

--- java-c#/ChatServer/src/chatserver/ChatServer.java
+++ java-c#/ChatServer/src/chatserver/ChatServer.java
@@ -1,15 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chatserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 /**
  *
  * @author aj3x
  */
-public class ChatServer {
+public class ChatServer implements Runnable{
+    private String name;
+    private boolean isrunning;
+    
+    private ServerSocket serverSocket;
+    private Socket clientSockets;
+
+    
+     public ChatServer(String name, int port){
+         try{
+             this.name = name;
+             this.serverSocket = new ServerSocket(port);
+             
+         }
+         catch(IOException e){
+             
+         }
+     }
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
 
     /**
      * @param args the command line arguments
@@ -17,5 +40,4 @@ public class ChatServer {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
 }

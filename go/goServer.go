75dfff459e764c6baaae5a9adb7df8276a7a5fc5
Alexander Russell (ajr546) <alexander.james@usask.ca>
2017-10-14 13:01:16 -0600

    added go server file

---
+++ go/goServer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"net"
+)
+
+
+type Client struct{
+	incoming chan string
+	outgoing chan string
+	reader *bufio.Reader
+	writer *bufio.Writer
+}
+
+
+
+
+
+
+type ChatRoom struct{
+	
+
+
+}
+
+
+
+
+func main() {
+
+
+
+}
